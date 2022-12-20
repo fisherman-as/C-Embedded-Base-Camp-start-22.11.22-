4bbc0a95bf64a1b06677b444956b0d0e53e951f3
Yabanzhi Aleksey <Aleksey.Yabanzhi@avtor.ua>
2022-12-20 12:46:53 +0200

    the program was simplified

--- Pure-C-Training-02-Basic-calculations/main.c
+++ Pure-C-Training-02-Basic-calculations/main.c
@@ -17,19 +17,6 @@ int main()
         double a=(double)((pi/12)*i);   //here we created the angle (a) for each counting with a step of 15 grades
         double z1=cos(a)+cos(2*a)+cos(6*a)+cos(7*a);
         double z2=4*cos(a/2)*cos(5*a/2)*cos(4*a);
-        char* end; //this variable keeps the ending, for comfortable reading of results after executing the program
-            if ((i+1)==1)
-                {
-                    end="st";
-                }
-            else if ((i+1)==2||(i+1)==3)
-                {
-                    end="d";
-                }
-            else
-                {
-                    end="th";
-                }
  /*
                         The PRINTF block:
         Here we print the result for each of 5-th countings.
