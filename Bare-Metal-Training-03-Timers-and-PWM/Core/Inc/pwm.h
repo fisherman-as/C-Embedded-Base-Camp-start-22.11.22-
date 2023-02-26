d33ac6a6bc6197db6086fb0657a2c3d8de9cf561
Yabanzhi Aleksey <Aleksey.Yabanzhi@avtor.ua>
2023-02-26 17:45:20 +0200

    the main function is redone

---
+++ Bare-Metal-Training-03-Timers-and-PWM/Core/Inc/pwm.h
@@ -0,0 +1,42 @@
+/*
+ * pwm.h
+ *
+ *  Created on: 18 лют. 2023 р.
+ *      Author: AS
+ */
+
+#ifndef INC_PWM_H_
+#define INC_PWM_H_
+
+#include "main.h"
+#include "stm32f4xx_hal_tim.h"
+
+typedef struct
+    {
+	  uint32_t Channel;     //channel number
+	  uint32_t Frequency;
+      uint32_t DutyCycle;
+    }PWM;
+typedef struct
+	{
+	  uint32_t SW1:10;
+	  uint32_t SW2:10;
+	  uint32_t SW3:10;
+	  uint32_t SW4:10;
+	  uint32_t SW5:10;
+	}BUTTONCOUNTER;
+	typedef enum
+	{None=0,SW1,SW2,SW3,SW4,SW5}BUTTON;
+
+void StopChannels(TIM_HandleTypeDef); //stops all 4 PWM channels
+void HandlePWM(BUTTON, BUTTONCOUNTER*, TIM_HandleTypeDef, PWM*);
+void SW1Handler(TIM_HandleTypeDef, PWM*);//this button decreases a frequency
+void SW2Handler(TIM_HandleTypeDef, PWM*);//this button selects the signal output
+void SW3Handler(TIM_HandleTypeDef, PWM*);//this button increases a frequency
+void SW4Handler(TIM_HandleTypeDef, PWM*);//this button increases the duty cycle
+void SW5Handler(TIM_HandleTypeDef, PWM*);//this button decreases the duty cycle
+void Tim4ReInit(TIM_HandleTypeDef, PWM*);//restart of the PWM with updated settings
+void ButtonsHandler(TIM_HandleTypeDef);  //the main function, it monitors...
+                                         //...PWM settings struct and restarts PWM
+
+#endif /* INC_PWM_H_ */
